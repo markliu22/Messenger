0a316a76202c6bbd12c484e214ea7d841f83762f
Mark Liu <markliu202@gmail.com>
2023-12-23 00:58:23 -0500

    Initial commit

---
+++ server/cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"server/db"
+)
+
+func main() {
+	dbConn, err := db.NewDatabase()
+	if err != nil {
+		log.Fatalf("could not initialize database connection: %s", err)
+	}
+}
---
+++ server/internal/user/user.go
@@ -0,0 +1,28 @@
+package user
+
+type User struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserReq struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserRes struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type Repository interface {
+	CreateUser(ctx context.Context, user *User) (*User, error)
+}
+
+type Service interface {
+	CreateUser(c context.Context, req *CreateUserReq) (*CreateUserRes, error)
+}
